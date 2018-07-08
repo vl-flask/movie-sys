aea46662086dff4fcb68865ec7e1a9d0c06ca373
Vassily <vasil.deville@gmail.com>
2018-07-08 10:33:27 +0300

    Provide CSV export

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from movie import Movie
 from user import User
+from movie import Movie
 
-user = User("Vassily")
-print(user)
-my_movie = Movie("The Matrix", "action", True)
-user.movies.append(my_movie)
+user = User('Vassily')
+user.add_movie("The Matrix", "Action")
+user.add_movie("The Interview", "Comedy")
+user.save2csv()
 
-print(f"User movies: {user.movies}")
-print(f"User watched: {user.watched_movies()}")
+# BEFORE WRITING TO CSV
+# from movie import Movie
+# from user import User
+#
+# user = User("Vassily")
+# print(user)
+# my_movie = Movie("The Matrix", "action", True)
+# user.movies.append(my_movie)
+#
+# print(f"User movies: {user.movies}")
+# print(f"User watched: {user.watched_movies()}")
--- user.py
+++ user.py
@@ -1,17 +1,17 @@
+from movie import Movie
 class User:
     """docstring for User."""
     def __init__(self, name):
         self.name = name
         self.movies = []
 
-    def add_movie(self, name, genre): # my_user_object.add_movie(name, genre)
-        movie = Movie(name, genre)
+    def add_movie(self, name, genre):
+        # my_user_object.add_movie(name, genre)
+        movie = Movie(name, genre, False)
         self.movies.append(movie)
 
     def delete_movie(self, name):
         self.movies = list(filter(lambda movie: movie.name!=name, self.movies))
-
-
 
     def watched_movies(self):
         wmovies = list(filter(lambda x: x.watched, self.movies))
@@ -21,6 +21,13 @@ class User:
         #         wmovies.append(m)
         return wmovies
 
-
     def __repr__(self):
         return f"<User {self.name}>"
+
+    def save2csv(self):
+        fname = f"{self.name}.txt"
+        with open(fname, 'w') as f:
+            f.write(self.name + "\n")
+            for movie in self.movies:
+                pattern = "{},{},{}\n"
+                f.write(pattern.format(movie.name, movie.genre, movie.watched))
